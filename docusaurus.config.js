15abd2258848a5293764c19d2e316d4d4cf66781
Damodar Lohani <dlohani48@gmail.com>
2020-10-20 11:41:42 +0545

    first commit

---
+++ docusaurus.config.js
@@ -0,0 +1,91 @@
+module.exports = {
+  title: 'Damodar Lohani',
+  tagline: 'Mobile and Web Developer',
+  url: 'https://dlohani.com.np',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  favicon: 'img/favicon.ico',
+  organizationName: 'lohanidamodar', // Usually your GitHub org/user name.
+  projectName: 'dlohani.com.np', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'Damodar Lohani',
+      logo: {
+        alt: 'Damodar Lohani',
+        src: 'img/logo.svg',
+      },
+      items: [
+        /* {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left',
+        }, */
+        { to: 'blog', label: 'Blog', position: 'left' },
+        {
+          href: 'https://github.com/lohanidamodar/dlohani.com.np',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
