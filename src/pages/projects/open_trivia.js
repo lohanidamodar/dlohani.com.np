cd597a9a90f30eb28df57c0d1a2fa9066375f92f
Damodar Lohani <dlohani48@gmail.com>
2020-10-24 07:06:49 +0545

    open trivia project added

---
+++ src/pages/projects/open_trivia.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ProjectWithLayout from './components/project-with-layout.js';
+
+const OpenTrivia = () => <ProjectWithLayout slug="open_trivia" />;
+export default OpenTrivia;
