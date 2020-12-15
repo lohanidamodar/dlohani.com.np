e4fbec3bbbb8a86b43e2d52f7ea017f834083795
Damodar Lohani <dlohani48@gmail.com>
2020-12-15 16:09:08 +0545

    adding testimonials section

---
+++ src/data/testimonials.js
@@ -0,0 +1,27 @@
+const testimonials = [
+    {
+        image: 'https://scontent.fktm4-1.fna.fbcdn.net/v/t1.0-9/128997422_472262273835724_1499080244182624626_n.jpg?_nc_cat=108&ccb=2&_nc_sid=09cbfe&_nc_ohc=4Gyr42p9ESwAX-PDH7X&_nc_ht=scontent.fktm4-1.fna&oh=c9888f04a862d73c6af85d59daab4aa5&oe=5FFEE5D4',
+        testimonial: `I am pleased to work with him and his attention, dedication to my Project. He explains everything in detail if I am confused anywhere related to my project.  I appreciate his detail and creative approach to bring my project to a new height. 
+        Thank you so much Mr. Damodar Lohani.`,
+        name: 'Birendra Bohara',
+        company: 'EPS-TOPIK Practice',
+        position: 'Chairman'
+    },
+    {
+        image: 'https://scontent.fktm4-1.fna.fbcdn.net/v/t1.0-9/128997422_472262273835724_1499080244182624626_n.jpg?_nc_cat=108&ccb=2&_nc_sid=09cbfe&_nc_ohc=4Gyr42p9ESwAX-PDH7X&_nc_ht=scontent.fktm4-1.fna&oh=c9888f04a862d73c6af85d59daab4aa5&oe=5FFEE5D4',
+        testimonial: `I am pleased to work with him and his attention, dedication to my Project. He explains everything in detail if I am confused anywhere related to my project.  I appreciate his detail and creative approach to bring my project to a new height. 
+        Thank you so much Mr. Damodar Lohani.`,
+        name: 'Birendra Bohara',
+        company: 'EPS-TOPIK Practice',
+        position: 'Chairman'
+    },
+    {
+        image: 'https://scontent.fktm4-1.fna.fbcdn.net/v/t1.0-9/128997422_472262273835724_1499080244182624626_n.jpg?_nc_cat=108&ccb=2&_nc_sid=09cbfe&_nc_ohc=4Gyr42p9ESwAX-PDH7X&_nc_ht=scontent.fktm4-1.fna&oh=c9888f04a862d73c6af85d59daab4aa5&oe=5FFEE5D4',
+        testimonial: `I am pleased to work with him and his attention, dedication to my Project. He explains everything in detail if I am confused anywhere related to my project.  I appreciate his detail and creative approach to bring my project to a new height. 
+        Thank you so much Mr. Damodar Lohani.`,
+        name: 'Birendra Bohara',
+        company: 'EPS-TOPIK Practice',
+        position: 'Chairman'
+    },
+];
+export default testimonials;
