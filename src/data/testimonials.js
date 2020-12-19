1169c544471daa17761e9e39c86ce766dfae1076
Damodar Lohani <dlohani48@gmail.com>
2020-12-19 16:05:34 +0545

    testimonial of hearth pizza

--- src/data/testimonials.js
+++ src/data/testimonials.js
@@ -8,20 +8,11 @@ const testimonials = [
         position: 'Chairman'
     },
     {
-        image: 'https://scontent.fktm4-1.fna.fbcdn.net/v/t1.0-9/128997422_472262273835724_1499080244182624626_n.jpg?_nc_cat=108&ccb=2&_nc_sid=09cbfe&_nc_ohc=4Gyr42p9ESwAX-PDH7X&_nc_ht=scontent.fktm4-1.fna&oh=c9888f04a862d73c6af85d59daab4aa5&oe=5FFEE5D4',
-        testimonial: `I am pleased to work with him and his attention, dedication to my Project. He explains everything in detail if I am confused anywhere related to my project.  I appreciate his detail and creative approach to bring my project to a new height. 
-        Thank you so much Mr. Damodar Lohani.`,
-        name: 'Birendra Bohara',
-        company: 'EPS-TOPIK Practice',
-        position: 'Chairman'
-    },
-    {
-        image: 'https://scontent.fktm4-1.fna.fbcdn.net/v/t1.0-9/128997422_472262273835724_1499080244182624626_n.jpg?_nc_cat=108&ccb=2&_nc_sid=09cbfe&_nc_ohc=4Gyr42p9ESwAX-PDH7X&_nc_ht=scontent.fktm4-1.fna&oh=c9888f04a862d73c6af85d59daab4aa5&oe=5FFEE5D4',
-        testimonial: `I am pleased to work with him and his attention, dedication to my Project. He explains everything in detail if I am confused anywhere related to my project.  I appreciate his detail and creative approach to bring my project to a new height. 
-        Thank you so much Mr. Damodar Lohani.`,
-        name: 'Birendra Bohara',
-        company: 'EPS-TOPIK Practice',
-        position: 'Chairman'
+        image: 'https://hearthpizza.com.au/wp-content/uploads/2017/12/23032740_1577671015627647_4182575104426973710_n-720x400.jpg',
+        testimonial: `We have worked with 'Damodar Lohani' to establish our website. They are ready to help anytime if we need to update anything on our website. Our website is very easy to understand/use and customers get everything they need in one click. We are very satisfied and highly recommend 'Damodar Lohani'.
+        Thank you for your service.`,
+        name: 'Krishna Subedi',
+        company: 'Hearth | Pizza & Small plates',
     },
 ];
 export default testimonials;
