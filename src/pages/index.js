b6641c48bbea77f84a0476ad7397899739dc55fd
Damodar Lohani <dlohani48@gmail.com>
2020-12-19 16:07:12 +0545

    fix testimonial layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -87,23 +87,25 @@ function RecentProjects() {
 }
 
 function Testimonials() {
-  return (<section className={clsx(styles.section, 'padding-vert--xl padding-horiz--lg container')}>
-    <div className="row">
-      {testimonials.slice(0, 2).map((testimonial, idx) => <div className="col col--6">
-        <div class="avatar avatar--vertical">
-          {testimonial.image && <img
-            class={clsx(styles.testimonialAvatar,'avatar__photo avatar__photo--xl')}
-            src={testimonial.image}
-          />}
-          <div class="avatar__intro">
-            <h4 class="avatar__name">{testimonial.name}</h4>
-            <small class="avatar__subtitle">
-              {testimonial.position && testimonial.position} {testimonial.company}
-            </small>
+  return (<section className={clsx(styles.section, 'padding-vert--xl padding-horiz--lg container--fluid')}>
+    <div className="container">
+      <div className="row">
+        {testimonials.slice(0, 2).map((testimonial, idx) => <div className="col col--6">
+          <div class="avatar avatar--vertical">
+            {testimonial.image && <img
+              class={clsx(styles.testimonialAvatar, 'avatar__photo avatar__photo--xl')}
+              src={testimonial.image}
+            />}
+            <div class="avatar__intro">
+              <h4 class="avatar__name">{testimonial.name}</h4>
+              <small class="avatar__subtitle">
+                {testimonial.position && testimonial.position} {testimonial.company}
+              </small>
+            </div>
           </div>
-        </div>
-        <p className="padding-top--md text--center">{testimonial.testimonial}</p>
-      </div>)}
+          <p className="padding-top--md text--center">{testimonial.testimonial}</p>
+        </div>)}
+      </div>
     </div>
   </section>)
 }
