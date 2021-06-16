87ce45d179c68a7bb1c8ed2ecc9c2becbe70dd81
Damodar Lohani <dlohani48@gmail.com>
2021-06-16 07:10:43 +0545

    update contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,6 +3,7 @@ import Layout from '@theme/Layout';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import clsx from 'clsx';
 import styles from './styles.module.css';
+import socials from '../data/socials';
 
 function Contact() {
     const context = useDocusaurusContext();
@@ -18,7 +19,15 @@ function Contact() {
                 </div>
             </header>
             <main>
-                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSf1imMuA15b38x5OGRznJ4kKK9jDZgUIkwmWCQOeRI8ollEMw/viewform?embedded=true" width="100%" height="1700" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+                {socials && socials.length > 0 && (
+                    <div className="container padding-top--lg">
+                        {socials.map(({ label, link }, idx) => (
+                            <p>
+                                <a key={idx} className="padding-bottom-sm padding-left--md" href={link} target="_blank">{label}</a>
+                            </p>
+                        ))}
+                    </div>
+                )}
             </main>
 
         </Layout>
