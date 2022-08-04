9cadee70cb32c9214f3eac040b5be4312a1b260f
Damodar Lohani <dlohani48@gmail.com>
2022-08-04 11:34:25 +0000

    update to latest docusaurus version

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,81 +1,97 @@
-module.exports = {
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
   title: 'Damodar Lohani',
   tagline: 'Mentor, Trainer & Tech Consultant',
   url: 'https://dlohani.com.np',
   baseUrl: '/',
   onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
   favicon: 'img/logo.svg',
   organizationName: 'lohanidamodar',
   projectName: 'lohanidamodar.github.io',
-  themeConfig: {
-    navbar: {
-      title: 'Damodar Lohani',
-      hideOnScroll: true,
-      logo: {
-        alt: 'Damodar Lohani',
-        src: 'img/logo.svg',
-      },
-      items: [
-        { to: 'projects', label: 'Projects', position: 'left' },
-        { to: 'talks', label: 'Talks', position: 'left' },
-        { to: 'blog', label: 'Blog', position: 'left' },
-        { to: 'contact', label: 'Contact', position: 'left' },
-        {
-          href: 'https://github.com/lohanidamodar',
-          position: 'right',
-          className: 'header-github-link',
-          'aria-label': 'GitHub repository',
-        },
-      ],
-    },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Socials',
-          items: [
-            {
-              label: 'Youtube',
-              href: 'https://youtube.com/c/reactbits',
-            },
-            {
-              label: 'Github',
-              href: 'https://github.com/lohanidamodar',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/lohanidamodar',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Projects',
-              to: 'projects',
-            },
-            {
-              label: 'Contact',
-              to: 'contact',
-            },
-            {
-              label: 'Blog',
-              to: 'blog',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} Damodar Lohani`,
-    },
-    prism: {
-      additionalLanguages: ['dart', 'yaml'],
-    },
+
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
   },
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Damodar Lohani',
+        hideOnScroll: true,
+        logo: {
+          alt: 'Damodar Lohani',
+          src: 'img/logo.svg',
+        },
+        items: [
+          { to: 'projects', label: 'Projects', position: 'left' },
+          { to: 'talks', label: 'Talks', position: 'left' },
+          { to: 'blog', label: 'Blog', position: 'left' },
+          { to: 'contact', label: 'Contact', position: 'left' },
+          {
+            href: 'https://github.com/lohanidamodar',
+            position: 'right',
+            className: 'header-github-link',
+            'aria-label': 'GitHub repository',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Socials',
+            items: [
+              {
+                label: 'Youtube',
+                href: 'https://youtube.com/c/reactbits',
+              },
+              {
+                label: 'Github',
+                href: 'https://github.com/lohanidamodar',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/lohanidamodar',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Projects',
+                to: 'projects',
+              },
+              {
+                label: 'Contact',
+                to: 'contact',
+              },
+              {
+                label: 'Blog',
+                to: 'blog',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Damodar Lohani`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+        additionalLanguages: ['dart', 'yaml'],
+      },
+    }),
   presets: [
     [
-      '@docusaurus/preset-classic',
-      {
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
         docs: false,
         blog: {
           showReadingTime: true,
@@ -89,7 +105,9 @@ module.exports = {
           trackingID: 'G-NR1FQ09LP2',
           anonymizeIP: true,
         }
-      },
+      }),
     ],
   ],
 };
+
+module.exports = config;
