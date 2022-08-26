176d999c3599a8ed2d290680c2c9764ca4d6ba4e
Damodar Lohani <dlohani48@gmail.com>
2022-08-26 06:33:41 +0000

    clients component

--- src/pages/_components/clients.js
+++ src/pages/_components/clients.js
@@ -2,13 +2,26 @@ import React from 'react'
 import clsx from 'clsx';
 import clients from '../../data/clients';
 import styles from '../styles.module.css'
+import useBaseUrl from '@docusaurus/useBaseUrl';
+
+
+function Client({ name, techStack, logo, link}) {
+    return(
+        <div>
+            <img src={useBaseUrl(logo)} alt={name} />
+            <p>{name}</p>
+        </div>
+    );
+}
 
 function Clients() {
     return (
         <section className={clsx(styles.section, 'padding-vert--xl padding-horiz--lg container')}>
-            <h2 className="text--center">Clients</h2>
-            <p className="text--center margin-bottom--lg">Some of my recent clients</p>
-            {clients.slice(0, 3).map((client, idx) => client.name )}
+            <h2 className="text--center">Recent Clients</h2>
+
+            <div className={styles.clients}>
+                {clients.slice(0, 3).map((client, idx) => <Client {...client} /> )}
+            </div>
         </section>
     );
 }
