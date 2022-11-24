e0939b6256e2a26507db89524b54373d6e1b3794
Damodar Lohani <dlohani48@gmail.com>
2022-11-24 02:20:33 +0000

    web book page

--- src/pages/_components/hero_flutter_web.js
+++ src/pages/_components/hero_flutter_web.js
@@ -6,16 +6,30 @@ import styles from '../styles.module.css'
 import Link from "@docusaurus/Link";
 
 function Hero() {
-    return (<header className={clsx('hero hero--secondary', styles.heroBanner, styles.heroBg, styles.heroFlutterWebBg)}>
-        <div className="overlay">
-            <div className="container--fluid">
-                <h1 className="hero__title">Taking Flutter To Web</h1>
-                <p className="hero__subtitle">Flutter web and much more.</p>
-                <div className={styles.buttons}>
+    return (<section className={clsx('hero hero--primary')}>
+        <div className="container">
+            <div className="row row--align-center">
+                <div className="col col--6 text--right">
+                    <img className={styles.bookImageLarge} src={useBaseUrl('img/projects/taking-flutter-to-web.jpg')} alt="Taking Flutter to Web" />
+                </div>
+
+                <div className="col col--6 text--left">
+                    <h1 className="hero__title">Taking Flutter To Web</h1>
+                    <p className="hero__subtitle">
+                    <ul>
+                        <li>Flutter Web</li>
+                        <li>Navigator 2.0</li>
+                        <li>Riverpod and MVVM</li>
+                        <li>Appwrite Integration</li>
+                        <li>Firebase Integration</li>
+                        <li>and more</li>
+                    </ul>
+
+                    </p>
                     <a href="https://www.amazon.in/Taking-Flutter-Web-cross-platform-platforms/dp/1801817715"
                         target="_blank"
                         className={clsx(
-                            'button button--primary button--lg',
+                            'button button--secondary button--lg',
                             styles.getStarted,
                         )}>
                         Get Your Copy
@@ -23,7 +37,7 @@ function Hero() {
                 </div>
             </div>
         </div>
-    </header>);
+    </section>);
 }
 
 export default Hero;
--- src/pages/flutter-web.js
+++ src/pages/flutter-web.js
@@ -2,34 +2,35 @@ import React from 'react';
 import clsx from 'clsx';
 import Layout from '@theme/Layout';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 import MDXContent from '@theme/MDXContent';
 import styles from './styles.module.css';
-import BlogTwo from '../featured/should-you-choose-flutter.mdx';
+import ToC from '../featured/flutter-web-toc.mdx';
 import Hero from './_components/hero_flutter_web';
 
 
 function Home() {
-  const context = useDocusaurusContext();
   return (
     <Layout
       title="Flutter Web Book"
       description="Flutter web, Flutter Firebase, Appwrite Integration, Navigator 2 and more book">
+        <head>
+        <meta name="keywords" content="flutter, flutter web," />
+        <meta property="og:image" content={useBaseUrl('img/projects/taking-flutter-to-web.jpg')} />
+      </head>
       <Hero />
       <main>
-        <section className={clsx('bg--primary', styles.intro, styles.section)}>
-          <div className="container">
-            <h2>What's Inside?</h2>
-            <ul>
-                <li>Flutter Web</li>
-                <li>Navigator 2.0</li>
-                <li>Riverpod and MVVM</li>
-                <li>Appwrite Integration</li>
-                <li>Firebase Integration</li>
-                <li>and more</li>
-            </ul>
+        <section className={clsx(styles.section, 'bg--primary')}>
+          <div className="container padding-vert--xl">
+            <div className="row row--align-center">
+              <div className="col col--6 text--left">
+                <MDXContent>
+                  <ToC />
+                </MDXContent>
+              </div>
+            </div>
           </div>
         </section>
-
       </main>
     </Layout>
   );
